bd8ce8ca19d64480fa0bdbdb09d291c2c2f4faef
eiheihei233 <90083814+eiheihei233@users.noreply.github.com>
2021-11-25 21:54:57 +0800

    Add files via upload

---
+++ readUsers/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+// 云函数入口函数
+//查询
+exports.main = async (event, context) => {
+  try {
+    //order
+    return await db.collection('users').where({
+      exist = true
+    }).get({
+      success: function (res) {
+       return res;
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
